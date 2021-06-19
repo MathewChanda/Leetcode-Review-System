3d397a934df66828377795d8f988c6894c75e0e5
Mathew Chanda <mvchanda@ncsu.edu>
2021-06-19 02:52:34 -0400

    starter code

---
+++ background.js
@@ -0,0 +1,9 @@
+let color = '#3aa757';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
+
+
+// this is a script that running in the background 
